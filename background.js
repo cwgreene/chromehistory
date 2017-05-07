35b138d587a3f47c45737bb5f3c7e48360d8190b
Chris Greene <archgoon@gmail.com>
2017-05-07 15:11:45 -0700

    Fixed tab checking.
    Using chrome.tabs.query and then using the chrome.runtime.id to
    ensure that the ui.html page is in fact the one we own.

--- background.js
+++ background.js
@@ -1,10 +1,34 @@
 let doom;
-chrome.browserAction.onClicked.addListener(function(initiatingTab) {
-  chrome.tabs.getCurrent((tab) => {
-    console.log("hi");
-    if (!tab) {
-      chrome.tabs.create({"url":"ui.html"});
-      const bp = chrome.extension.getBackgroundPage();
+
+function logAllVisits() {
+  chrome.history.query({"text":""}, (historyItems) => {
+    const results = [];
+    let count = 0;
+    let done = () => {
+      console.log(results);
     }
+    historyItems.map((item) => {
+      chrome.history.getVisits({url: item.url}, (visit) => {
+        results.append(visit);
+        count += 1;
+        if (count == historyItems.length) {
+          done();
+        }
+      })});
   });
+}
+
+chrome.browserAction.onClicked.addListener(function(initiatingTab) {
+  chrome.tabs.query({
+      "url":`chrome-extension://${chrome.runtime.id}/ui.html`
+    },
+    (tabs) => {
+      console.log(tabs);
+      if (tabs.length == 0) {
+        chrome.tabs.create({"url":"ui.html"});
+      }
+      // Don't need to wait for tab creation, as the background page
+      // already exists.
+      const bp = chrome.extension.getBackgroundPage();
+    });
 });
