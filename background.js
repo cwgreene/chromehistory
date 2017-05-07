234dfe43e513ac9117de188af9a179e2ac920280
Chris Greene <archgoon@gmail.com>
2017-05-07 14:38:03 -0700

    Background tab now only opens once.

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({"url":"ui.html"});
+let doom;
+chrome.browserAction.onClicked.addListener(function(initiatingTab) {
+  chrome.tabs.getCurrent((tab) => {
+    console.log("hi");
+    if (!tab) {
+      chrome.tabs.create({"url":"ui.html"});
+      const bp = chrome.extension.getBackgroundPage();
+    }
+  });
 });
